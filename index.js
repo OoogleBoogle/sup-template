d90769305f6bd7c06c38a2f9b105bf10b4d449ff
OoogleBoogle <simattfield@gmail.com>
2016-05-02 12:07:34 -0700

    Users tests passing

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ app.get('/users', function(req, res) {
     });
 });
 
-app.post('/users', jsonParser, function(req, res) {
-    var user = new User({
-        username: req.body.username
-    });
-
-    user.save(function(err, user) {
-        if (err) {
-            return res.sendStatus(500);
-        }
-
-        return res.status(201).location('/users/' + user._id).json({});
-    });
-});
-
-
 app.get('/users/:userId', function(req, res) {
     User.findOne({
         _id: req.params.userId
@@ -41,8 +26,69 @@ app.get('/users/:userId', function(req, res) {
         if (err) {
             return res.sendStatus(500);
         }
-
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
         return res.json(user);
+    });
+});
+
+app.post('/users', jsonParser, function(req, res) {
+    if (!('username' in req.body)) {
+      return res.status(422).json({message: 'Missing field: username'})
+    }
+
+    if (typeof req.body.username !== 'string') {
+        return res.status(422).json({message: 'Incorrect field type: username'});
+    }
+
+    if (req.body.username === "") {
+        return res.status(422).json({message: 'Empty field: username'});
+    }
+
+    var user = new User({
+        username: req.body.username
+    });
+
+    user.save(function(err, user) {
+        if (err) {
+            return res.sendStatus(422);
+        }
+
+        return res.status(201).location('/users/' + user._id).json({});
+    });
+});
+
+app.put('/users/:userId', jsonParser, function(req, res) {
+    if (!('username' in req.body)) {
+      return res.status(422).json({message: 'Missing field: username'})
+    }
+
+    if (typeof req.body.username !== 'string') {
+        return res.status(422).json({message: 'Incorrect field type: username'});
+    }
+
+    if (req.body.username === "") {
+        return res.status(422).json({message: 'Empty field: username'});
+    }
+
+    User.findByIdAndUpdate(req.params.userId, req.body, {upsert: true}, function(err) {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        return res.status(200).json({});
+    });
+});
+
+app.delete('/users/:userId', jsonParser, function(req, res) {
+    User.findByIdAndRemove(req.params.userId, function(err, user) {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        if (!user) {
+          return res.status(404).json({message: 'User not found'});
+        }
+        return res.status(200).json({});
     });
 });
 
@@ -54,4 +100,3 @@ mongoose.connect(databaseUri).then(function() {
 });
 
 module.exports = app;
-
